d64863e244cdd54a987b81a09893d57be19c0b60
99cone2 <99cone2@naver.com>
2023-09-21 17:32:25 +0900

    9.21 페이징

--- meat_rule/src/main/java/com/project/gogi/vo/Criteria.java
+++ meat_rule/src/main/java/com/project/gogi/vo/Criteria.java
@@ -1,22 +1,22 @@
 package com.project.gogi.vo;
 
 public class Criteria {
-// Criteria는 rNum의 제한값과  현재 페이지,페이지에 출력되는 게시물 숫자를 제어
+// Criteria는 Limit 현재 페이지,페이지에 출력되는 게시물 숫자를 제어
 	//시작값과 끝값을 다루는 클래스
 	
-	 private int page;  //페이지 
-	 private int perPageNum; //한 화면에 출력되는 게시물의 숫자
-	 private int rowStart; //rNum 제한값중 시작값
-	 private int rowEnd;	////rNum 제한값중 끝값
+	 private int page;  //페이지 //페이지 번호
+	 private int perPageNum; //한 화면에 출력되는 게시물의 숫자 //페이지당 게시글 갯수
+	 private int rowStart; //페이지 한 행의 첫번째 게시물 rowNum
+	 private int rowEnd;	//페이지 한 행의 마지막 게시물 rowNum
 	 
-	 public Criteria()
+	 public Criteria()// 기본 생성자
 	 {
-	  this.page = 1;
-	  this.perPageNum = 10;
+	  this.page = 1; //페이지 1로 초기화
+	  this.perPageNum = 10; //페이지당 게시글 10개
 	 }
 
 	 
-	 public void setPage(int page)
+	 public void setPage(int page)// 페이지 번호 set
 	 {
 	  if (page <= 0)
 	  {
@@ -26,7 +26,7 @@ public class Criteria {
 	  this.page = page;
 	 }
 
-	 // 한 화면에 출력되는 게시물의 숫자/ 관련 메서드
+	 // 한 화면에 출력되는 게시물의 숫자/ 관련 메서드 // 페이지당 게시물 개수 set
 	 public void setPerPageNum(int perPageNum)
 	 {
 	  if (perPageNum <= 0 || perPageNum > 100)
@@ -45,7 +45,7 @@ public class Criteria {
 
 	 //특정 페이지의 게시글 시작 번호, 게시글 시작 행 번호
 	 //현재 페이지의 게시글 시작 번호 = (현재 페이지 번호 - 1) * 페이지당 보여줄 게시글 갯수
-	 //getPerPageNum = (this.page - 1) * perPageNum
+	 //현재 페이지의 페이지당 게시글 수를 곱하여 현재 페이지의 시작 게시글 limit 수를 구하는것(Mysql) 
 	 public int getPageStart()
 	 {
 	  return (this.page - 1) * perPageNum;
